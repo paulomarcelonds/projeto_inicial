15ef41d156ca49c04dafcb8b6e95ae4a800d5f7e
Paulo <paulomarcelonds@gmail.com>
2024-07-25 13:54:23 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print('começando com python')
+
+# Variáveis
+idade = 27
+nome = 'Juliano'
+
+# Print
+print('idade')
+
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+print(lista)
+
+# Metódos
+lista_invertida = lista.reverse()
+print(lista_invertida)
